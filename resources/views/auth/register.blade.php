d1690fbb795f2aa318b08b3674865ee506d3bb28
mxxxnxxx <mmm.mt.co.jp@gmail.com>
2020-09-14 21:18:28 +0900

    DBツールSequelProへ接続+ageをredisterのviewに追記

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -25,6 +25,21 @@
                             </div>
                         </div>
 
+                        {{--  ageのフォーム  --}}
+                        <div class="form-group row">
+                            <label for="age" class="col-md-4 col-form-label text-md-right">{{ __('Age') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('age') is-invalid @enderror" name="age" value="{{ old('age') }}" required autocomplete="age" autofocus>
+
+                                @error('age')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
