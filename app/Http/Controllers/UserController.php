bb1d332d8e95561813f8140ba05c4d3e1b15eb1b
mxxxnxxx <mmm.mt.co.jp@gmail.com>
2020-09-17 23:27:34 +0900

    マイページ用のuserコントローラにview表示を記述

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,13 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    //ユーザーページ表示を行う記述
+
+    // 以下でユーザー情報をにゅうしゅしている
+    $user = Auth::user();
+    
+    // ユーザ情報を元にviewを表示
+    return view('user.index',['auth' => $user]);
+    
     
 }
