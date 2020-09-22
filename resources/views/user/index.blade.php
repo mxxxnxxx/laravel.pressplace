d4366da390f58f6444bed970c2c1e29fc8530ec4
mxxxnxxx <mmm.mt.co.jp@gmail.com>
2020-09-22 21:40:37 +0900

    user/index.blade.php作成

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,7 +1,45 @@
 {{--  @extends('layouts.app')  --}}
 @extends('layouts.parent')
-@section('title','top')
+@section('title', Auth::user()->name . 'のページ')
 @include('layouts.head')
-@include('layouts.header')    
+@include('layouts.header')
 @include('layouts.footer')
+
+@section('content')
+    <div class="container">
+      <a href="編集ページへのリンク" class="profile_editing">プロフィール編集</a>
+      <div class="user_image"></div>
+
+      <div class="user_name"></div>
+      
+      <div class="user_introduction"></div>
+        {{--  @if (ユーザーの自己紹介がある時)  --}}
+        {{--  else  --}}
+        {{--  <a href="編集ページへのリンク">自己紹介を書く</a>  --}}
+        {{--  @endif  --}}
+
+      <div class="user_pressplace">
+        {{--  @if (ユーザーのpressがある時)  --}}
+        {{--  prees内容が繰り返しで並ぶ  --}}
+          {{--  @foreach ($places as $place)  --}}
+              {{--  <div class=""></div>  --}}
+                
+
+              {{--  以下でdbより自分の投稿のみを取得  --}}
+                  {{--  @yield('pressplace')  --}}
+
+          {{--  @endforeach  --}}
+
+        {{--  @else  --}}
+        {{--  pressがありません  --}}
+        {{--  @endif  --}}
+      </div>
+      
+      <div class="paginate">
+      {{--  ↓以下はページネーション　10投稿ずつ  --}}
+      {{--  {{ $posts->links() }}  --}}
+      </div>
+
+    </div>
+@endsection
 
