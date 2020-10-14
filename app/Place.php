fa695087e01c9b167974fcbcfe95f27f3986dcb9
mxxxnxxx <mmm.mt.co.jp@gmail.com>
2020-10-14 20:28:57 +0900

    ソフトデリート一対多

--- app/Place.php
+++ app/Place.php
@@ -3,18 +3,25 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class Place extends Model
 {
+    // ソフトデリート
     use SoftDeletes;
-
     protected $dates = ['deleted_at'];
-
-    return $this->belongsTo('App\User');
+    
+    
     
     protected $fillable = [
         'name',
         'comment',
         'place_image',
     ];
+
+
+// placeがuserに属す
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
+
 }
