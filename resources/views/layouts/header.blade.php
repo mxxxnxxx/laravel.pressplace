03360243e250f54e8cfd7213fdbc8614b3f658ee
mxxxnxxx <mmm.mt.co.jp@gmail.com>
2020-10-22 10:32:40 +0900

    place投稿へのリンク作成

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,11 +6,13 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    PressPlace
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
+
+                
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
@@ -21,6 +23,7 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
+                        {{--  ログインしていない時  --}}
                         @guest
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
@@ -30,13 +33,19 @@
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
+                        {{--  以下がログインしている時のみ  --}}
                         @else
+                        {{--  新規投稿ボタン  --}}
+                        <div>
+                            <a href={{ route('place.new') }} class='btn btn-outline-primary'>新しいお店</a>
+                        </div>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    
                                     {{--  マイページへのリンク  --}}
                                     <a class="dropdown-item" href={{ route('user.show',['user' => auth()->user()->id]) }}>
                                         マイページ
@@ -48,7 +57,6 @@
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
-
                                     
                                     
                                     {{--  以下が実際ログアウトさせている記述  --}}
